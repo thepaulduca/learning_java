31a07e0ba9d996c4b133ebca4d2c8028d776956f
Paul Duca <paul.r.duca@gmail.com>
2017-03-21 12:20:16 -0500

    change from entire string to just word

--- PigLatin.java
+++ PigLatin.java
@@ -4,36 +4,26 @@ public class PigLatin {
 
     Scanner scanner = new Scanner(System.in);
 
-
-    System.out.println("Hello, please enter some text:");
+    final String vowels = "aeiouAEIOU";
+    System.out.println("Hello, please enter a word to convert to Pig Latin");
     String input = scanner.nextLine();
-    System.out.println("You entered:" + input);
-    
+    System.out.println("You entered: " + input);
+
     String piglatinstr = new String("");
+    String firstLetter = new String("");
 
-    for(int i = 0; i < str.length(); i++) {
-
-      char myChar = str.charAt(i);
-
-      if(myChar == ' '|| i == 0) {
-
-        String firstLetter = new String("");
-
-        if(i == 0) {
-          firstLetter = firstLetter.concat(Character.toString(str.charAt(i)));
-          System.out.print(firstLetter);
-        } else {
-          firstLetter = firstLetter.concat(Character.toString(str.charAt(i + 1)));
-          System.out.print(firstLetter);
-        }
-
-        piglatinstr = piglatinstr + firstLetter;
-        // System.out.println(firstLetter);
+    for(int i = 0; i < input.length(); i++) {
+      if(i == 0) {
+        firstLetter = firstLetter.concat(Character.toString(input.charAt(i)));
       } else {
-        String letter = Character.toString(str.charAt(i));
-        piglatinstr.concat(letter);
+        String letter = Character.toString(input.charAt(i));
+        piglatinstr = piglatinstr.concat(letter);
       }
-      // System.out.println(piglatinstr);
+
     }
+
+
+
+    System.out.println(piglatinstr);
   }
 }
